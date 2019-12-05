5e9846d53b3d6269ea1aeb1eb363ecb18e5ec4a8
AlexHLane <unconfigured@null.spigotmc.org>
2019-12-04 21:23:35 -0600

    First real commit try

---
+++ ALaneLibrary/ALaneLibrary/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALaneLibrary.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime DueDate { get; set; }
+        public List<string> OnHoldList { get; set; }
+
+    }
+}
